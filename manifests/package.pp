d1563476ce97849d95467d17990abccb9535bee1
Carl Alexander <carlalexander@gmail.com>
2013-02-18 16:24:20 -0500

    Still trying to get ordering right

--- manifests/package.pp
+++ manifests/package.pp
@@ -20,18 +20,19 @@ class dome9::package {
     mode  => '0644',
   }
 
-  exec { 'dome9.list':
-    command => "/bin/echo 'deb http://repository.dome9.com/ubuntu lucid main' >> /etc/apt/sources.list.d/dome9.list",
-    creates => '/etc/apt/sources.list.d/dome9.list'
+  exec { 'dome9-key.asc':
+    command => '/usr/bin/wget -O - http://repository.dome9.com/ubuntu/dome9-key.asc | /usr/bin/apt-key add -',
+    unless  => '/usr/bin/apt-key list | /bin/grep -c dome9-key.asc',
   }
 
-  exec { 'dome9-key.asc':
-    command => '/usr/bin/wget -O - http://repository.dome9.com/ubuntu/dome9-key.asc | /usr/bin/apt-key add - && /usr/bin/apt-get update;',
-    unless  => '/usr/bin/apt-key list | /bin/grep -c dome9-key.asc',
+  exec { 'dome9.list':
+    command => "/bin/echo 'deb http://repository.dome9.com/ubuntu lucid main' >> /etc/apt/sources.list.d/dome9.list && /usr/bin/apt-get update",
+    creates => '/etc/apt/sources.list.d/dome9.list',
+    require => Exec['dome9-key.asc']
   }
 
   package { 'dome9agent':
     ensure  => latest,
-    require => [Exec['dome9-key.asc'], Exec['dome9.list']]
+    require => Exec['dome9.list']
   }
 }
